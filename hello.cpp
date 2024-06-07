07f0094f56962ec6251652667fc971bf50cea05b
hrk84ya <hrk84ya@gmail.com>
2024-06-08 00:18:40 +0530

    First Program

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//First program
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!\n";
+    int myAge=19;
+    cout << "I am " << myAge << " years old";
+    return 0;
+}
