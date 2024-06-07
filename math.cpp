625d064d6b16b742576c02e6982caf7c07c80715
hrk84ya <hrk84ya@gmail.com>
2024-06-08 00:19:14 +0530

    Using cmath library

---
+++ math.cpp
@@ -0,0 +1,11 @@
+//Using the math library
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    cout << sqrt(64)<<"\n";
+    cout << round(2.7)<<"\n";
+    cout << max(10,11);
+    return 0;
+}
