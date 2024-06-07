aaec8e5bb88007766bb9a1ba65be5e684de3a43e
hrk84ya <hrk84ya@gmail.com>
2024-06-08 00:21:08 +0530

    Switch statement application

---
+++ switch.cpp
@@ -0,0 +1,36 @@
+//Using the switch statement
+#include <iostream>
+using namespace std;
+
+int main(){
+    int day;
+    cout << "Enter the day: ";
+    cin >> day;
+
+    switch(day){
+        case 1:
+            cout<<"Today is Monday";
+            break;
+        case 2:
+            cout<<"Today is Tuesday";
+            break;
+        case 3:
+            cout<<"Today is Wednesday";
+            break;
+        case 4:
+            cout<<"Today is Thursday";
+            break;
+        case 5:
+            cout<<"Today is Friday";
+            break;
+        case 6:
+            cout<<"Today is Saturday";
+            break;
+        case 7:
+            cout<<"Today is Sunday";
+            break;
+        default:
+            cout<<"Invalid input";
+    }
+    return 0;
+}
