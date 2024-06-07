377fc1a958388fffc5eee1fe25d172ed7a97c237
hrk84ya <hrk84ya@gmail.com>
2024-06-08 00:22:15 +0530

    Using while loop

---
+++ while.cpp
@@ -0,0 +1,13 @@
+//Utilizing the while loop
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num=10;
+    while(num>0){
+        cout<<num<<"\n";
+        num--;
+    }
+    return 0;
+}
