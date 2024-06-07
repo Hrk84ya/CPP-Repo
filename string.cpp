498305606efd645cd5cfe0866faef00f317fc9a0
hrk84ya <hrk84ya@gmail.com>
2024-06-08 00:20:20 +0530

    String library application

---
+++ string.cpp
@@ -0,0 +1,35 @@
+//Utilizing the string library
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << "!\n";
+
+    string firstName = "Harsh ";
+    string lastName = "Kumar";
+
+    string fullName = firstName.append(lastName);
+    cout << fullName<<"\n";
+
+    //using the length function to get the length of a string
+    cout << "The length of fullName is: "<<fullName.length()<<"\n";
+
+    //using the at function to access character of a string
+    cout << fullName.at(1)<<"\n";
+
+    //Clear the input buffer
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    //Taking an input line
+    string favCharacter;
+    cout << "Enter your favorite character: ";
+    getline(cin, favCharacter);
+    cout << "Your favorite character is: "<<favCharacter;
+
+    return 0;
+}
