0d05e99ccc44a92542eb276e49a473bbd93d504a
hrk84ya <hrk84ya@gmail.com>
2024-06-08 00:17:40 +0530

    Using bool variable type

---
+++ boolean.cpp
@@ -0,0 +1,11 @@
+//Using boolean variable type
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool isCodingFun=true;
+    bool isFishTasty=false;
+    cout << isCodingFun<<"\n";
+    cout << isFishTasty;
+    return 0;
+}
