c99be734db500000c98e498bbcfe29e14b1896ff
hrk84ya <hrk84ya@gmail.com>
2024-06-09 17:45:14 +0530

    Implement a while loop for iterative control

--- while.cpp
+++ while.cpp
@@ -1,13 +1,15 @@
-//Utilizing the while loop
-
+// Including the necessary header file for input-output operations
 #include <iostream>
 using namespace std;
 
-int main(){
-    int num=10;
-    while(num>0){
-        cout<<num<<"\n";
-        num--;
+int main() {
+    int num = 10; // Initializing a variable 'num' with the value 10
+
+    // Using a while loop to count down from 10 to 1
+    while (num > 0) {
+        cout << num << "\n"; // Outputting the value of 'num' followed by a newline
+        num--; // Decrementing the value of 'num' by 1 in each iteration
     }
+
     return 0;
 }
