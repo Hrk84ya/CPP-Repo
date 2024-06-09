de230fb798eb7b59ccc77b08f5a2cb4ca5a2cd9e
hrk84ya <hrk84ya@gmail.com>
2024-06-09 17:34:19 +0530

    Implement user input handling and arithmetic operations

--- UserInput.cpp
+++ UserInput.cpp
@@ -1,23 +1,29 @@
-//Using the cin function to take input from the user
-
+// Including the necessary header file for input-output operations
 #include <iostream>
 using namespace std;
 
-int main(){
-    int num1,num2;
+int main() {
+    int num1, num2; // Declaring variables to store the entered numbers
+
+    // Prompting the user to enter the first number
     cout << "Enter a number: ";
-    cin >> num1;
+    cin >> num1; // Reading the user input and storing it in 'num1'
 
+    // Prompting the user to enter the second number
     cout << "Enter another number: ";
-    cin >> num2;
+    cin >> num2; // Reading the user input and storing it in 'num2'
 
-    int sum=num1+num2;
-    int diff=num1-num2;
-    int prod=num1*num2;
-    int quot=num1/num2;
+    // Performing arithmetic operations
+    int sum = num1 + num2;    // Addition
+    int diff = num1 - num2;   // Subtraction
+    int prod = num1 * num2;   // Multiplication
+    int quot = num1 / num2;   // Integer division
 
-    cout << "Sum of "<<num1<<" and "<<num2<<" is: "<<sum << "\n";
-    cout << "Difference of "<<num1<<" and "<<num2<<" is: "<<diff << "\n";
-    cout << "Product of "<<num1<<" and "<<num2<<" is: "<<prod<<"\n";
-    cout << "Quotient of "<<num1<<" and "<<num2<<" is: "<<quot;
+    // Outputting the results of the arithmetic operations
+    cout << "Sum of " << num1 << " and " << num2 << " is: " << sum << "\n";
+    cout << "Difference of " << num1 << " and " << num2 << " is: " << diff << "\n";
+    cout << "Product of " << num1 << " and " << num2 << " is: " << prod << "\n";
+    cout << "Quotient of " << num1 << " and " << num2 << " is: " << quot;
+
+    return 0;
 }
