c959ec579159aa8db5705fd478ebfa155bcb36ec
hrk84ya <hrk84ya@gmail.com>
2024-06-09 17:31:41 +0530

    Implement pointer usage and value manipulation

--- Pointer.cpp
+++ Pointer.cpp
@@ -1,17 +1,29 @@
+// Including the necessary header files for input-output operations and string manipulations
 #include <iostream>
 #include <string>
 using namespace std;
 
-int main(){
-    string food="Pizza";
-    string *ptr=&food;
+int main() {
+    // Initializing a string variable with the value "Pizza"
+    string food = "Pizza";
 
-    cout <<food<<"\n"; //output: Pizza
-    cout <<ptr<<"\n"; //output: address of the variable
-    cout <<*ptr<<"\n"; //output: content of the variable
+    // Declaring a pointer to a string and assigning it the address of the variable 'food'
+    string *ptr = &food;
 
-    *ptr="Noodles"; //changing the content of the variable using pointer
-    cout <<*ptr<<"\n";
+    // Output the value of the variable 'food'
+    cout << food << "\n"; // Output: Pizza
+
+    // Output the memory address stored in the pointer 'ptr' (address of the variable 'food')
+    cout << ptr << "\n"; // Output: address of the variable
+
+    // Output the value pointed to by the pointer 'ptr' (content of the variable 'food')
+    cout << *ptr << "\n"; // Output: Pizza
+
+    // Changing the content of the variable 'food' using the pointer 'ptr'
+    *ptr = "Noodles";
+
+    // Output the new value pointed to by the pointer 'ptr' (updated content of the variable 'food')
+    cout << *ptr << "\n"; // Output: Noodles
 
     return 0;
 }
