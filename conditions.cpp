c5e0cea5e590737564398006767e0b740f764e94
hrk84ya <hrk84ya@gmail.com>
2024-06-09 17:37:16 +0530

    Implement age-based conditional statements

--- conditions.cpp
+++ conditions.cpp
@@ -1,18 +1,25 @@
-//Using the condition statements
+// Including the necessary header file for input-output operations
 #include <iostream>
 using namespace std;
 
-int main(){
-    int age;
-    cout << "Enter your age: ";
-    cin >> age;
+int main() {
+    int age; // Declaring a variable to store the age
 
-    if (age<13){
-        cout<<"You are a child";
-    }else if(age<18){
-        cout<<"You are a teenager";
-    }else{
-        cout<<"You are an adult";
+    // Prompting the user to enter their age
+    cout << "Enter your age: ";
+    cin >> age; // Reading the user input and storing it in the variable 'age'
+
+    // Using conditional statements to determine the life stage based on the age
+    if (age < 13) {
+        // If the age is less than 13, the person is considered a child
+        cout << "You are a child";
+    } else if (age < 18) {
+        // If the age is between 13 and 17 (inclusive), the person is considered a teenager
+        cout << "You are a teenager";
+    } else {
+        // If the age is 18 or more, the person is considered an adult
+        cout << "You are an adult";
     }
+
     return 0;
 }
