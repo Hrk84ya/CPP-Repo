7e6ee2afaccf2eaa7ffcf5a0f932ef27ad890592
hrk84ya <hrk84ya@gmail.com>
2024-06-09 17:42:06 +0530

    Demonstrate string manipulation functionalities

--- string.cpp
+++ string.cpp
@@ -1,35 +1,47 @@
-//Utilizing the string library
-
+// Including the necessary header files for input-output operations and string manipulations
 #include <iostream>
 #include <string>
 using namespace std;
 
-int main(){
+int main() {
+    // Declaring a string variable 'name' to store user's name
     string name;
+    
+    // Prompting the user to enter their name
     cout << "Enter your name: ";
-    cin >> name;
+    cin >> name; // Reading the user input and storing it in the variable 'name'
+    
+    // Greeting the user with their name
     cout << "Hello, " << name << "!\n";
 
+    // Declaring and initializing two string variables 'firstName' and 'lastName'
     string firstName = "Harsh ";
     string lastName = "Kumar";
 
+    // Concatenating 'firstName' and 'lastName' to form 'fullName'
     string fullName = firstName.append(lastName);
-    cout << fullName<<"\n";
+    cout << fullName << "\n";
 
-    //using the length function to get the length of a string
-    cout << "The length of fullName is: "<<fullName.length()<<"\n";
+    // Using the length function to get the length of the string 'fullName'
+    cout << "The length of fullName is: " << fullName.length() << "\n";
 
-    //using the at function to access character of a string
-    cout << fullName.at(1)<<"\n";
+    // Using the at function to access the character at index 1 in the string 'fullName'
+    cout << fullName.at(1) << "\n";
 
-    //Clear the input buffer
+    // Clearing the input buffer
     cin.ignore();
 
-    //Taking an input line
+    // Declaring a string variable 'favCharacter' to store user's favorite character
     string favCharacter;
+    
+    // Prompting the user to enter their favorite character
     cout << "Enter your favorite character: ";
+    
+    // Reading a line of input (allowing spaces) and storing it in the variable 'favCharacter'
     getline(cin, favCharacter);
-    cout << "Your favorite character is: "<<favCharacter;
+    
+    // Displaying the user's favorite character
+    cout << "Your favorite character is: " << favCharacter;
 
     return 0;
 }
