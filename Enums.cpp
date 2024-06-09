af3eef5aabdafd68073edfe85fbb063c70dddedb
hrk84ya <hrk84ya@gmail.com>
2024-06-09 17:29:04 +0530

    Add switch statement to handle different enum cases

--- Enums.cpp
+++ Enums.cpp
@@ -1,26 +1,34 @@
+// Including the necessary header files for input-output operations and string manipulations
 #include <iostream>
 #include <string>
 using namespace std;
 
-int main(){
-    enum Level{
+int main() {
+    // Defining an enumeration named 'Level' to represent different levels
+    enum Level {
         Low,
         Medium,
         High
     };
     
-    Level myLevel=Medium;
+    // Declaring a variable 'myLevel' of type 'Level' and initializing it with the value 'Medium'
+    Level myLevel = Medium;
 
-    switch(myLevel){
+    // Using a switch statement to perform actions based on the value of 'myLevel'
+    switch(myLevel) {
         case Low:
-            cout <<"Low level\n";
+            // If 'myLevel' is 'Low', print "Low level"
+            cout << "Low level\n";
             break;
         case Medium:
-            cout <<"Medium level\n";
+            // If 'myLevel' is 'Medium', print "Medium level"
+            cout << "Medium level\n";
             break;
         case High:
-            cout <<"High level\n";
+            // If 'myLevel' is 'High', print "High level"
+            cout << "High level\n";
             break;
     }
+
     return 0;
 }
