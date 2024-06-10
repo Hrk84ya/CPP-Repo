6c3bcced1ab5da596439efaf6b4dc52b24d20508
hrk84ya <hrk84ya@gmail.com>
2024-06-10 15:42:29 +0530

    Utilize fstream library for file operations

---
+++ File.cpp
@@ -0,0 +1,15 @@
+// Including the necessary header file for input-output and file operations
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    // Create and open a text file
+    ofstream MyFile("Alpha.txt");
+
+    // Write to the file
+    MyFile << "Files can be tricky, but it is fun enough!";
+
+    // Close the file
+    MyFile.close();
+}
