00587b89e2a1efaaffa1464e16f14b75395ac2d4
hrk84ya <hrk84ya@gmail.com>
2024-06-13 23:42:57 +0530

    Allocate Minimum Number of Pages from N books to M students

---
+++ minPages.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countStudents(vector<int> &arr, int pages) {
+    // Count the number of students needed to allocate books given the maximum number of pages per student
+    int n = arr.size(); 
+    int students = 1;
+    long long pagesStudent = 0;
+    for (int i = 0; i < n; i++) {
+        if (pagesStudent + arr[i] <= pages) {
+            pagesStudent += arr[i];
+        }
+        else {
+            students++;
+            pagesStudent = arr[i];
+        }
+    }
+    return students;
+}
+
+int findPages(vector<int>& arr, int n, int m) {
+    // If the number of students is greater than the number of books, allocation is impossible
+    if (m > n) return -1;
+
+    // Find the range for binary search
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int students = countStudents(arr, mid);
+        if (students > m) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> arr = {25, 46, 28, 49, 24};
+    int n = 5;
+    int m = 4;
+    int ans = findPages(arr, n, m);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
