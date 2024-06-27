cd77b8281c7cd997e4f0c53781a4f1b07a6c0919
hrk84ya <hrk84ya@gmail.com>
2024-06-27 23:23:00 +0530

    Finding sum of two linked list

---
+++ sumLinkedList.cpp
@@ -0,0 +1,46 @@
+//Given two linked lists, result a list containing the sum of the two
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = dummy;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+            cur->next = new ListNode(sum % 10);
+            cur = cur->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        return dummy->next;
+    }
+};
+
+int main() {
+    Solution s;
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+    ListNode* res = s.addTwoNumbers(l1, l2);
+    while (res) {
+        cout << res->val << " ";
+        res = res->next;
+    }
+    cout << endl;  
+    return 0;
+}
+
