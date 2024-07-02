f4a912b505570b1f5749e38643c56589a08ed779
hrk84ya <hrk84ya@gmail.com>
2024-07-02 11:24:26 +0530

    Shift Operator in C++

---
+++ shiftOperator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Left shift
+    int a=1;
+    int b=a<<1;
+
+    //Right shift
+    int c=2;
+    int d=c>>1;
+
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "c = " << c << ", d = " << d << endl;
+
+    return 0;
+}
